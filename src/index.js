a5d9583187cbb8cfdc5fac705ee4e4b07ff98b07
bobro <bobrow.bobro2015@yandex.ru>
2020-02-20 16:18:33 +0300

    feat: do all work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0)
+  	return 0;
+  var min = Infinity;
+  for(var i = 0; i < array.length; i++)
+  	if(min > array[i])
+  		min = array[i];
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0)
+  	return 0;
+  var max = -Infinity;
+  for(var i = 0; i < array.length; i++)
+  	if(max < array[i])
+  		max = array[i];
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0)
+  	return 0;
+  var sum = 0;
+  for(var i = 0; i < array.length; i++)
+  	sum += array[i];
+  return sum/array.length;
 }
